3e16afc0874fefb3c2b9f554abf1e11c0c0caa6f
Nozila <nozilarajabboyeva@gmail.com>
2026-03-27 17:22:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#6-misol
+m6 = lambda a, b: a * b
+result1 = m6(122, 211)
+print(result1)
+
+#7-misol
+m7 = lambda x, y: x // y
+
+res = m7(4, 2)
+print(res)
+
+#8-misol
+m8 = lambda x: x + 10
+
+res = m8(8)
+print(res)
+
+#9-misol
+m9 = lambda x: x - 5
+
+res = m9(76)
+print(res)
+
+#10-misol
+m10 = lambda x: x / 3
+
+res = m10(8765)
+print(res)
